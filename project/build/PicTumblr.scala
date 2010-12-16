4bfde42092d49985cfa06736b53c2d6cc472c071
motemen <motemen@gmail.com>
2010-12-17 02:24:37 +0900

    android-7

--- project/build/PicTumblr.scala
+++ project/build/PicTumblr.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 trait Defaults {
-  def androidPlatformName = "android-2.1"
+  def androidPlatformName = "android-7"
 }
 class PicTumblr(info: ProjectInfo) extends ParentProject(info) {
   override def shouldCheckOutputDirectories = false
