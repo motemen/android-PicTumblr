fc31b53f21b18c04f2d66051b68ed0b2681a4979
motemen <motemen@gmail.com>
2010-12-17 03:35:28 +0900

    test

--- project/build/PicTumblr.scala
+++ project/build/PicTumblr.scala
@@ -3,6 +3,7 @@ import sbt._
 trait Defaults {
   def androidPlatformName = "android-7"
 }
+
 class PicTumblr(info: ProjectInfo) extends ParentProject(info) {
   override def shouldCheckOutputDirectories = false
   override def updateAction = task { None }
--- src/test/scala/Specs.scala
+++ src/test/scala/Specs.scala
@@ -1,10 +1,16 @@
-import net.tokyoenvious.droid.pictumblr
+import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
-import org.scalatest.Spec
 
-class Specs extends Spec with ShouldMatchers {
-  describe("a spec") {
-    it("should do something") {
+import net.tokyoenvious.droid.pictumblr.Tumblr
+
+class TumblrSpec extends FlatSpec with ShouldMatchers {
+    "Valid authentication" should "succeed" in {
+        val email    = System.getenv("TUMBLR_EMAIL")
+        val password = System.getenv("TUMBLR_PASSWORD")
+
+        Tumblr.authenticate(email, password) should equal (true)
     }
-  }
+    "Invalid authentication" should "fail" in {
+        Tumblr.authenticate("", "") should equal (false)
+    }
 }
