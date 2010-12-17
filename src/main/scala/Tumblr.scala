b12ba13b6b68582f65459f08a552108b33b2bbd2
motemen <motemen@gmail.com>
2010-12-17 23:07:18 +0900

    とちゅう

--- src/main/scala/Tumblr.scala
+++ src/main/scala/Tumblr.scala
@@ -4,26 +4,29 @@ import java.net.URL
 import java.net.HttpURLConnection
 import java.io.OutputStreamWriter
 import scala.io.Source
+import scala.xml.XML
 
 object Tumblr {
     val API_ROOT = "http://www.tumblr.com/api/"
 
-    def authenticate (email : String, password : String) : Boolean = {
+    // とりあえずタイトルを返す
+    def authenticate (email : String, password : String) : Option[String] = {
         val url = new URL("http://www.tumblr.com/api/authenticate")
         val http = url.openConnection.asInstanceOf[HttpURLConnection]
         http.setRequestMethod("POST")
         http.setDoOutput(true)
         http.connect
 
-        val writer = new OutputStreamWriter(http.getOutputStream)
-        writer.write("email=" + email + "&password=" + password) // FIXME
-        writer.close
+        try {
+            val writer = new OutputStreamWriter(http.getOutputStream)
+            writer.write("email=" + email + "&password=" + password) // FIXME
+            writer.close
 
-        // TODO
+            // TODO
 
-        val in = Source.fromInputStream(http.getInputStream)
-        println(in.getLines.toList)
-
-        return false
+            return (XML.load(http.getInputStream) \ "tumblelog").elements.next.attribute("title").map(_.text)
+        } catch {
+            case _ => return None
+        }
     }
 }
