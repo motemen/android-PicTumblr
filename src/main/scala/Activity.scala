d7b5e250dad9863eb6314249eace0d7e0f912d0d
motemen <motemen@gmail.com>
2010-12-19 23:26:23 +0900

    古いの削除

--- src/main/scala/Activity.scala
+++ src/main/scala/Activity.scala
@@ -3,16 +3,34 @@ package net.tokyoenvious.droid.pictumblr
 import android.app.Activity
 import android.os.Bundle
 import android.widget.Toast
+import android.view.Menu
+import android.view.MenuItem
+import android.content.Intent
 import android.preference.PreferenceManager
 
 class PicTumblrActivity extends Activity {
+    val MENU_ITEM_ID_SETTING = Menu.FIRST + 1
+
     override def onCreate(savedInstanceState : Bundle) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
+    }
 
-        val prefs = PreferenceManager.getDefaultSharedPreferences(this)
-        val email = prefs.getString("email", "")
-        val password = prefs.getString("password", "")
-        Toast.makeText(this, email+":"+password, Toast.LENGTH_SHORT).show
+    override def onCreateOptionsMenu(menu : Menu) : Boolean = {
+        val itemSetting = menu.add(Menu.NONE, MENU_ITEM_ID_SETTING, Menu.NONE, "Setting")
+        itemSetting.setIcon(android.R.drawable.ic_menu_preferences)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override def onOptionsItemSelected(menuItem : MenuItem) : Boolean = {
+        menuItem.getItemId() match {
+            case MENU_ITEM_ID_SETTING => {
+                val intent = new Intent(this, classOf[PicTumblrPrefernceActivity])
+                startActivity(intent)
+            }
+        }
+
+        return true
     }
 }
