87b60aafe4916da47cf40992e82ba73c959e80ae
motemen <motemen@gmail.com>
2010-12-22 02:24:45 +0900

    メソッド整理

--- src/main/scala/Tumblr.scala
+++ src/main/scala/Tumblr.scala
@@ -5,7 +5,16 @@ import scala.xml.XML
 import java.net.URL
 import java.net.URLEncoder
 
+// 2.8 API
+class SeqExtra[A] (seq : Seq[A]) {
+    def reduceLeftOption[B >: A](op: (B, A) => B): Option[B] =
+        if (seq.isEmpty) None else Some(seq.reduceLeft(op))
+}
+
 class Tumblr (email : String, password : String) {
+    implicit def seq2extra[A] (seq: Seq[A]) : SeqExtra[A]
+        = new SeqExtra[A](seq)
+
     val API_ROOT = "http://www.tumblr.com/api/"
     val maxWidth = 500
 
@@ -14,19 +23,9 @@ class Tumblr (email : String, password : String) {
 
     // とりあえずタイトルを返す
     def authenticate () : Option[String] = {
-        val url = new URL(API_ROOT + "authenticate")
-        val http = url.openConnection.asInstanceOf[java.net.HttpURLConnection]
-        http.setRequestMethod("POST")
-        http.setDoOutput(true)
-        http.connect
-
         try {
-            val writer = new java.io.OutputStreamWriter(http.getOutputStream)
-            writer.write(mkPostString())
-            writer.close
-
             // API lv7 でえらる…… (SDK のバグ？)
-            return (XML.load(http.getInputStream) \ "tumblelog").elements.next.attribute("title").map(_.text)
+            return makeApiRequest("authenticate").elements.next.attribute("title").map(_.text)
         } catch {
             case e => {
                 Log.d("Tumblr.authenticate", e.toString)
@@ -36,32 +35,17 @@ class Tumblr (email : String, password : String) {
     }
 
     def dashboard () : Seq[Post] = {
-        Log.d("Tumblr.dashboard", "Requesting " + API_ROOT + "dashboard")
-
-        var url = new URL(API_ROOT + "dashboard")
-        val http = url.openConnection.asInstanceOf[java.net.HttpURLConnection]
-        http.setRequestMethod("POST")
-        http.setDoOutput(true)
-        http.connect
-
-        val writer = new java.io.OutputStreamWriter(http.getOutputStream)
-        writer.write(mkPostString('type -> "photo"))
-        writer.close
-
-        ( XML.load(http.getInputStream) \ "posts" \ "post" )
+        ( makeApiRequest("dashboard", ('type -> "photo")) \ "posts" \ "post" )
             .map {
-                postElem => (postElem \ "photo-url").foldLeft(None.asInstanceOf[Option[scala.xml.Node]]) {
-                    (node1_, node2) => node1_ match {
-                        case None => Some(node2)
-                        case Some(node1) => {
-                            val w1 = getPhotoUrlNodeMaxWidth(node1)
-                            val w2 = getPhotoUrlNodeMaxWidth(node2)
-                            if (w1 > w2) Some(node1) else Some(node2)
-                        }
+                postElem => (postElem \ "photo-url").reduceLeftOption {
+                    (node1, node2) => {
+                        val w1 = getPhotoUrlNodeMaxWidth(node1)
+                        val w2 = getPhotoUrlNodeMaxWidth(node2)
+                        if (w1 > w2) node1 else node2
                     }
                 }
             }
-            .filter { _ isDefined } .map { _.get }
+            .filter { _.isDefined } .map { _.get }
             .map {
                 photoUrlNode => PhotoPost(photoUrlNode.text)
             }
@@ -70,8 +54,9 @@ class Tumblr (email : String, password : String) {
     private def getPhotoUrlNodeMaxWidth (node : scala.xml.Node) =
         ( node \ "@max-width" ).firstOption map { _.text.toInt } filter { _ < maxWidth } getOrElse(0)
 
-    /*
     private def makeApiRequest (function : String, params : (Symbol, String)*) : scala.xml.Elem = {
+        Log.d("Tumblr.makeApiRequest", "Requesting " + API_ROOT + function)
+
         var url = new URL(API_ROOT + function)
         val http = url.openConnection.asInstanceOf[java.net.HttpURLConnection]
         http.setRequestMethod("POST")
@@ -79,12 +64,11 @@ class Tumblr (email : String, password : String) {
         http.connect
 
         val writer = new java.io.OutputStreamWriter(http.getOutputStream)
-        writer.write(mkPostString(params))
+        writer.write(mkPostString(params : _*))
         writer.close
 
         return XML.load(http.getInputStream)
     }
-    */
 
     private def mkPostString (params : (Symbol, String)*) : String = {
         ( Map('email -> email, 'password -> password) ++ params )
