b793d6dd415a773cb93f450ac45f59ee4044a0d0
motemen <motemen@gmail.com>
2010-12-22 01:45:03 +0900

    maxWidth 制限

--- src/main/scala/Tumblr.scala
+++ src/main/scala/Tumblr.scala
@@ -1,12 +1,13 @@
 package net.tokyoenvious.droid.pictumblr
 
 import android.util.Log
+import scala.xml.XML
 import java.net.URL
 import java.net.URLEncoder
-import scala.xml.XML
 
 class Tumblr (email : String, password : String) {
     val API_ROOT = "http://www.tumblr.com/api/"
+    val maxWidth = 500
 
     abstract class Post
     case class PhotoPost (photoUrl : String) extends Post
@@ -48,8 +49,26 @@ class Tumblr (email : String, password : String) {
         writer.close
 
         ( XML.load(http.getInputStream) \ "posts" \ "post" )
-            .map(postElem => PhotoPost((postElem \ "photo-url").first.text))
+            .map {
+                postElem => (postElem \ "photo-url").foldLeft(None.asInstanceOf[Option[scala.xml.Node]]) {
+                    (node1_, node2) => node1_ match {
+                        case None => Some(node2)
+                        case Some(node1) => {
+                            val w1 = getPhotoUrlNodeMaxWidth(node1)
+                            val w2 = getPhotoUrlNodeMaxWidth(node2)
+                            if (w1 > w2) Some(node1) else Some(node2)
+                        }
+                    }
+                }
+            }
+            .filter { _ isDefined } .map { _.get }
+            .map {
+                photoUrlNode => PhotoPost(photoUrlNode.text)
+            }
     }
+
+    private def getPhotoUrlNodeMaxWidth (node : scala.xml.Node) =
+        ( node \ "@max-width" ).firstOption map { _.text.toInt } filter { _ < maxWidth } getOrElse(0)
 
     /*
     private def makeApiRequest (function : String, params : (Symbol, String)*) : scala.xml.Elem = {
