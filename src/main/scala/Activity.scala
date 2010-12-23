a2bb7e83fa7ac0cb9fb867eb7043456b42554b21
motemen <motemen@gmail.com>
2010-12-23 12:28:50 +0900

    case match with inner class

--- src/main/scala/Activity.scala
+++ src/main/scala/Activity.scala
@@ -88,7 +88,7 @@ class PicTumblrActivity extends Activity {
         try {
             // XXX クロージャなんか渡して大丈夫なんだろうか…
             val task = new LoadDashboardTask(tumblr, layout, this.toast(_))
-            task.execute("1")
+            task.execute(1)
         } catch {
             case e => {
                 Log.e("PicTumblrActivity.updateDashboard", e.toString)
@@ -105,15 +105,16 @@ class PicTumblrActivity extends Activity {
     }
 }
 
-// FIXME AsyncTask[Int, ...] だと落ちる
+// AsyncTask[Int, ...] だと落ちる → java.lang.Integer に
 class LoadDashboardTask (tumblr : Tumblr, viewGroup : ViewGroup, toast : String => Unit)
-        extends AsyncTask1[String, java.lang.Void, Seq[Tumblr#Post]] {
+        extends AsyncTask1[java.lang.Integer, java.lang.Void, Seq[Tumblr#Post]] {
 
     override def onPreExecute () {
         toast("Loading dashboard...")
     }
 
-    override def doInBackground (page : String) : Seq[Tumblr#Post] = {
+    // 可変長引数でやりとりできないのは AsyncTask1.java にブリッジさせる
+    override def doInBackground (page : java.lang.Integer) : Seq[Tumblr#Post] = {
         Log.d("LoadDashboardTask", "doInBackground")
         return tumblr.dashboard()
     }
@@ -121,25 +122,31 @@ class LoadDashboardTask (tumblr : Tumblr, viewGroup : ViewGroup, toast : String 
     override def onPostExecute (posts : Seq[Tumblr#Post]) {
         toast("Dashboard loaded.")
 
-        // FIXME post match { case Tumblr#Post(url) => ... } できない
+        // post match { case Tumblr#PhotoPost(url) => ... } できない件は
+        // post match { case tumblr.PhotoPost(url) => ... } でいける
+        // ref. http://stackoverflow.com/questions/1812695/scala-case-class-matching-compile-error-with-aliased-inner-types
         posts foreach (
-            post => {
-                Log.d("LoadDashboardTask", "post: " + post.toString())
+            _ match {
+                case tumblr.PhotoPost(url) => {
+                    //val url = post.asInstanceOf[Tumblr#PhotoPost].photoUrl
+                    Log.d("LoadDashboardTask", "photoUrl: " + url)
 
-                val url = post.asInstanceOf[Tumblr#PhotoPost].photoUrl
-                Log.d("LoadDashboardTask", "photoUrl: " + url)
+                    val imageView = new ImageView(viewGroup.getContext())
+                    viewGroup.addView(imageView)
 
-                val imageView = new ImageView(viewGroup.getContext())
-                viewGroup.addView(imageView)
-
-                val task = new LoadPhotoTask(imageView)
-                task.execute(url)
+                    val task = new LoadPhotoTask(imageView)
+                    task.execute(url)
+                }
+                case post => {
+                    Log.d("LoadDashboardTask", "cannot handle post: " + post.toString())
+                }
             }
         )
     }
 }
 
 class LoadPhotoTask (imageView : ImageView) extends AsyncTask1[String, java.lang.Void, Bitmap] {
+    // 単純に Drawable.createFromStream() するとメモリを食うので Bitmap.Config.RGB_565 を指定
     override def doInBackground (url : String) : Bitmap = {
         val options = new BitmapFactory.Options
         options.inPreferredConfig = Bitmap.Config.RGB_565
