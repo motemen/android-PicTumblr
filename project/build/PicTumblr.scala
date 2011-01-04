627adccce39b000f4b4c5e2e2b143b7c7642cae3
motemen <motemen@gmail.com>
2011-01-05 00:22:50 +0900

    trait AutoRestartADBDaemon

--- project/build/PicTumblr.scala
+++ project/build/PicTumblr.scala
@@ -5,36 +5,35 @@ trait Defaults {
   def androidPlatformName = "android-7"
 }
 
+trait AutoRestartAdbDaemon extends AndroidProject {
+
+    override def adbTask (emulator : Boolean, action : String) = {
+        restartAdbServer(emulator)
+        super.adbTask(emulator, action)
+    }
+
+    def restartAdbServer (emulator : Boolean) {
+        if (emulator) {
+            if (! (<_> {adbPath.absolutePath} -e get-state </_>.lines_! exists { _ == "device" })) {
+                ( <_> {adbPath.absolutePath} kill-server </_> ## <_> {adbPath.absolutePath} start-server </_> ) !
+            }
+        }
+    }
+}
+
 class PicTumblr(info: ProjectInfo) extends ParentProject(info) {
   override def shouldCheckOutputDirectories = false
   override def updateAction = task { None }
 
-  lazy val main  = project(".",     "PicTumblr", new MainProject(_))
-  lazy val tests = project("tests", "tests",     new TestProject(_), main)
+  lazy val main  = project(".",     "PicTumblr - app",   new MainProject(_))
+  lazy val tests = project("tests", "PicTumblr - tests", new TestProject(_), main)
 
-  class MainProject(info: ProjectInfo) extends AndroidProject(info) with Defaults with MarketPublish {
+  class MainProject(info: ProjectInfo) extends AndroidProject(info) with Defaults with MarketPublish with AutoRestartAdbDaemon {
     val keyalias  = "change-me"
     // val scalatest = "org.scalatest" % "scalatest" % "1.0" % "test"
-
-    override def adbTask (emulator : Boolean, action : String) = {
-        restartAdbServer(emulator).run
-        super.adbTask(emulator, action)
-    }
-
-    def restartAdbServer (emulator : Boolean) = {
-      if (emulator) {
-        execTask {
-          (adbPath.absolutePath + " devices") #| "grep emulator" #|| {
-            <x> {adbPath.absolutePath} kill-server </x> #&& <x> {adbPath.absolutePath} devices </x>
-          }
-        }
-      } else {
-        task { None }
-      }
-    }
   }
 
-  class TestProject(info: ProjectInfo) extends AndroidTestProject(info) with Defaults {
+  class TestProject(info: ProjectInfo) extends AndroidTestProject(info) with Defaults with AutoRestartAdbDaemon {
     override def proguardInJars = runClasspath --- proguardExclude
     val scalatest = "org.scalatest" % "scalatest" % "1.0"
   }
