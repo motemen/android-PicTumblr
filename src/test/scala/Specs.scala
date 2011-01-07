2d6709e6d29cb6ec6e1d4ea27f702912f4714814
motemen <motemen@gmail.com>
2011-01-07 19:17:44 +0900

    いらないのを削除

--- src/test/scala/Specs.scala
+++
@@ -1,9 +0,0 @@
-import org.scalatest.FlatSpec
-import org.scalatest.matchers.ShouldMatchers
-
-import net.tokyoenvious.droid.pictumblr.Tumblr
-
-class TaskGroupSpec extends FlatSpec with ShouldMatchers {
-    "Callback" should "be called" in {
-    }
-}
